53060d75c5b021ee28b39bec9050967096fed6ca
Jesús Espino <jesus.espino@kaleidos.net>
2013-10-07 23:20:39 +0200

    Adding actions i_want and i_talk

--- backend/intranet/api/talks.py
+++ backend/intranet/api/talks.py
@@ -3,7 +3,9 @@
 from django.conf import settings
 
 from rest_framework import permissions
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import detail_route
 
 from intranet import models
 from intranet import serializers
@@ -15,3 +17,15 @@ class TalkViewSet(ModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     paginate_by = settings.API_DEFAULT_PAGE_SIZE
     paginate_by_param = "page_size"
+
+    @detail_route(methods=["post"])
+    def i_want(self, request, pk=None, format=None):
+        talk = self.get_object()
+        talk.wanters.add(request.user)
+        return Response({"detail": u"Talk marked as wanted."})
+
+    @detail_route(methods=["post"])
+    def i_talk(self, request, pk=None, format=None):
+        talk = self.get_object()
+        talk.talkers.add(request.user)
+        return Response({"detail": u"Talk marked me as talker."})
