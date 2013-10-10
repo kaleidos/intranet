adce011987353029981909008ea9a26ab67bd9a3
Jesús Espino <jesus.espino@kaleidos.net>
2013-10-10 17:39:24 +0200

    Fixing url problems

--- backend/intranet/urls.py
+++ backend/intranet/urls.py
@@ -1,12 +1,14 @@
 
 from django.conf.urls import *
 
+from django.conf import settings
+
 from intranet.views.admin import *
 from intranet.api.routers import router, auth_router
 
 urlpatterns = patterns('',
-    url(r'^project_summary/(?P<project_id>\d+)/$', ProjectSummary.as_view(), name="project-summary"),
-    url(r'^holidays_summary/$', HolidaysSummary.as_view(), name="holidays-summary"),
-    url(r'^', include(router.urls)),
-    url(r'^', include(auth_router.urls)),
+    url(r'^%s/project_summary/(?P<project_id>\d+)/$' % (settings.BASE_ADMIN,), ProjectSummary.as_view(), name="project-summary"),
+    url(r'^%s/holidays_summary/$' % (settings.BASE_ADMIN,), HolidaysSummary.as_view(), name="holidays-summary"),
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/', include(auth_router.urls)),
 )
--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,6 @@
 from django.conf.urls import *
 from django.contrib import admin
 
-import django.contrib.auth.views
 from django.conf import settings
 from intranet.feeds import HolidaysRequestFeed
 
@@ -10,11 +9,10 @@ from intranet.feeds import HolidaysRequestFeed
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^api/v1/', include('intranet.urls')),
+    url(r'^', include('intranet.urls')),
 
-    url(r'^%s/extra/'%(settings.BASE_ADMIN), include('intranet.urls.admin', namespace='admin-extra')),
-    url(r'^%s/'%(settings.BASE_ADMIN), include(admin.site.urls)),
-    url(r'^%s/autoreports/'%(settings.BASE_ADMIN), include('autoreports.urls')),
+    url(r'^%s/extra/' % (settings.BASE_ADMIN,), include('intranet.urls', namespace='admin-extra')),
+    url(r'^%s/autoreports/' % (settings.BASE_ADMIN,), include('autoreports.urls')),
 
     url(r'^passwd/password_reset/$', 'django.contrib.auth.views.password_reset'),
     url(r'^passwd/password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
