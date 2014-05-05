bfcfe064e67c040941501f4edad075867ea1aee5
David Barragán Merino <david.barragan@kaleidos.net>
2014-05-05 23:28:26 +0200

    Create a superuser at DB regeneration

--- backend/regenerate.sh
+++ backend/regenerate.sh
@@ -5,4 +5,4 @@ python manage.py migrate
 python manage.py sample_data
 echo "DELETE FROM auth_permission;" | python manage.py dbshell
 echo "DELETE FROM django_content_type;" | python manage.py dbshell
-
+python manage.py createsuperuser
